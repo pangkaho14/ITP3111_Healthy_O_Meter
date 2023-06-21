84772d87d5e8ec25850ecf0ed5627d047b8ad04f
personryan <mageryan1@gmail.com>
2023-06-21 19:12:47 +0800

    forget to save the unity file xD

--- HealthyOMeter/Assets/Code/Scripts/laneManager.cs
+++ HealthyOMeter/Assets/Code/Scripts/laneManager.cs
@@ -6,6 +6,10 @@ public class LaneManager : MonoBehaviour
 {
     public Transform[] lanes; // Array of lane GameObjects
     public float laneChangeSpeed = 5f; // Speed of lane change
+    public int threshold = 10; // Threshold of swipe strength
+
+    public float swipeBufferTime = 0.5f; // Cooldown time between lane changes
+    private bool canSwipe = true; // Flag to track if a swipe is currently allowed
 
     public Transform player; // Reference to the player object
 
@@ -14,25 +18,32 @@ public class LaneManager : MonoBehaviour
     private void Update()
     {
         // Detect swipe input
-        if (Input.touches.Length > 0)
+        if (canSwipe && Input.touches.Length > 0)
         {
             Touch touch = Input.touches[0];
             if (touch.phase == TouchPhase.Moved)
             {
                 float swipeDelta = touch.deltaPosition.x;
+                if (Mathf.Abs(swipeDelta) >= threshold)
+                {
+                    // Process the swipe
+                    // Determine swipe direction
+                    int targetLaneIndex = currentLaneIndex;
+                    if (swipeDelta < 0) // Swipe left
+                        targetLaneIndex--;
+                    else if (swipeDelta > 0) // Swipe right
+                        targetLaneIndex++;
 
-                // Determine swipe direction
-                int targetLaneIndex = currentLaneIndex;
-                if (swipeDelta < 0) // Swipe left
-                    targetLaneIndex--;
-                else if (swipeDelta > 0) // Swipe right
-                    targetLaneIndex++;
+                    // Clamp the target lane index within the valid range
+                    targetLaneIndex = Mathf.Clamp(targetLaneIndex, 0, lanes.Length - 1);
 
-                // Clamp the target lane index within the valid range
-                targetLaneIndex = Mathf.Clamp(targetLaneIndex, 0, lanes.Length - 1);
+                    // Move the player to the target lane
+                    MoveToLane(targetLaneIndex);
 
-                // Move the player to the target lane
-                MoveToLane(targetLaneIndex);
+                    // Apply buffer - Disable swiping temporarily
+                    StartCoroutine(EnableSwipeBuffer());
+                }
+                
             }
         }
     }
@@ -49,6 +60,15 @@ public class LaneManager : MonoBehaviour
         //player.position = Vector3.Lerp(player.position, targetPosition, Time.deltaTime * laneChangeSpeed);
         // Example of direct positioning:
         player.position = targetPosition;
+    }
+
+    private IEnumerator EnableSwipeBuffer()
+    {
+        canSwipe = false; // Disable swiping
+
+        yield return new WaitForSeconds(swipeBufferTime);
+
+        canSwipe = true; // Enable swiping again
     }
 }
 
