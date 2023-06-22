2abbc7481d8e7928b2db49f4e826c1c9ee61b06a
personryan <mageryan1@gmail.com>
2023-06-22 14:43:24 +0800

    feat: gameover scene
    gameover background
    play again button and logic

---
+++ HealthyOMeter/Assets/Code/Scripts/gameOverBtn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class gameOverBtn : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        Button buttonStartAgain = root.Q<Button>("btn_playAgain");
+        buttonStartAgain.clicked += LoadNextScene;
+    }
+
+    private void LoadNextScene()
+    {
+        // Replace "NextSceneName" with the name or index of your desired next scene
+        SceneManager.LoadScene("Lanes");
+    }
+}
