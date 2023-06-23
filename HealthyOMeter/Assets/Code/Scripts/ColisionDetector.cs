86f5053f3604ba481d287f89bbd1ab463a090b9c
personryan <mageryan1@gmail.com>
2023-06-23 14:40:38 +0800

    feat: develop death logic
    Coded player hp logic

--- HealthyOMeter/Assets/Code/Scripts/ColisionDetector.cs
+++ HealthyOMeter/Assets/Code/Scripts/ColisionDetector.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class ColisionDetector : MonoBehaviour
 {
+    public int damageAmount = 10; // The amount of HP to be reduced
+
     private void OnCollisionEnter2D(Collision2D collision)
-    {
+    {   
+        //When hit by the food
+        PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
+        
+        if (playerHealth != null)
+        {
+            playerHealth.ReduceHP(damageAmount);
+            playerHealth.CheckHP();
+        }
+
         Destroy(gameObject);
     }
 }
