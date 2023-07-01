df377763bf8bdb78c2c4cb3014fd79ff097882c3
Timothy <92201555+timothxy@users.noreply.github.com>
2023-07-01 11:55:53 +0800

    feat: dynamic character selection
    dynamic character selection and fixed settings page

--- HealthyOMeter/Assets/Code/Scripts/ColisionDetector.cs
+++ HealthyOMeter/Assets/Code/Scripts/ColisionDetector.cs
@@ -4,34 +4,6 @@ public class ColisionDetector : MonoBehaviour
 {
     public int damageAmount = 10; // The amount of HP to be reduced
 
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {   
-    //     //When hit by the food
-    //     PlayerHealth playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
-        
-    //     if (playerHealth != null)
-    //     {
-    //         playerHealth.ReduceHP(damageAmount);
-    //         playerHealth.CheckHP();
-    //     }
-    //     // if (collision.collider.name == "icecreamdrop(Clone)"){
-    //     //     Debug.Log("icecreamdrop");
-    //     //     if (playerHealth != null)
-    //     //     {
-    //     //         playerHealth.ReduceHP(damageAmount);
-    //     //         playerHealth.CheckHP();
-    //     //     }
-    //     // }
-    //     // else
-    //     // {
-    //     //     playerHealth.GainHP(damageAmount);
-    //     //     playerHealth.CheckHP();
-    //     //     Debug.Log(collision.collider.name);
-    //     // }
-
-    //     Destroy(gameObject);
-    // }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
         //Playerhealth affected when food collide with player
