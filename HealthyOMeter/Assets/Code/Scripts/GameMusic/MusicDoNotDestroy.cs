5c44e301478d6937e0af045d4a2f61de899abfb8
Timothy <92201555+timothxy@users.noreply.github.com>
2023-07-15 18:47:12 +0800

    fix: translation for pause title and nutri-grade

--- HealthyOMeter/Assets/Code/Scripts/GameMusic/MusicDoNotDestroy.cs
+++ HealthyOMeter/Assets/Code/Scripts/GameMusic/MusicDoNotDestroy.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.Audio;
 
 public class MusicDoNotDestroy : MonoBehaviour
 {
+    [SerializeField] public AudioMixer SoundMixer;
+
     private void Awake()
     {
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("gamemusic");
@@ -13,5 +16,17 @@ public class MusicDoNotDestroy : MonoBehaviour
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
+
+        if (PlayerPrefs.HasKey("SoundVolume"))
+        {
+            float volume = PlayerPrefs.GetFloat("SoundVolume");
+            SoundMixer.SetFloat("Sound", Mathf.Log10(volume)*20);
+        }
+        else
+        {
+            // float volume = PlayerPrefs.GetFloat("SoundVolume");
+            // SoundMixer.SetFloat("Sound", Mathf.Log10(volume)*20);
+            // PlayerPrefs.SetFloat("SoundVolume", volume);
+        }
     }
 }
